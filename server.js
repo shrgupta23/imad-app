012092eacccc4771793e3333e071f0035ad226e5
shrgupta23 <30551530+shrgupta23@users.noreply.github.com>
2018-02-24 18:35:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,15 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-
+app.get('/articleone',function(req,res){
+  res.send('Article one Requested and will be served here');
+});
+app.get('/articletwo',function(req,res){
+  res.send('Article two Requested and will be served here');
+});
+app.get('/articlethree',function(req,res){
+  res.send('Article three Requested and will be served here');
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
