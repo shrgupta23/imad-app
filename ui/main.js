0e422c807309b3c81427c82f70ab910deb897fc6
shrgupta23 <30551530+shrgupta23@users.noreply.github.com>
2018-02-26 20:05:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,14 @@
-console.log('Loaded!');
-//change the text of main Html Element
-var element=document.getElementById('main-text');
-element.innerHtml ='New Value';
+//counter code
+var Button=document.getElementById();
+var counter=0;
+button.onClick(){
+    //make a request to counter end point
+    
+    //store it in avariabl
+    
+    //render the variable in correct span
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
+    
+}
