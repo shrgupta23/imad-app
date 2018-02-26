e447b2d2c24d2535b8714b8392a036938c42c84a
shrgupta23 <30551530+shrgupta23@users.noreply.github.com>
2018-02-26 19:54:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -58,6 +58,11 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 var counter=0;
+app.get('/counter', function (req, res) {
+  counter=counter+1;
+  res.send(counter.toString());});
+
+
 
 app.get('/:articleName',function(req,res){//feature of express framework
 
